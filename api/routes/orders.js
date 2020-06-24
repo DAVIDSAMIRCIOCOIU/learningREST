ff850ce648fe0847b122b9a86f4bd9dca074b341
DAVIDCIOCOIU95 <david.samir.ciocoiu@gmail.com>
2020-06-24 14:12:28 +0200

    error handling added in app.js!

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,4 +7,28 @@ router.get('/', (req, res, next) => {
     })
 });
 
+router.post('/', (req, res, next) => {
+    // 201 is "success, resource created"
+    res.status(201).json({
+        message: 'Orders was created'
+    })
+});
+
+// Individual orders
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: 'Order details',
+        id: id
+    })
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: 'Order deleted',
+        id: id 
+    })
+});
+
 module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'));
 // Use routes
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes)
+
+//Error handling middleware: create the error
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    // Forward the error requests
+    next(error);
+});
+// Catch all kind of errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
