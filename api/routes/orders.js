87479f83eda17856779e97ba16bffd23ffbd680c
DAVIDCIOCOIU95 <david.samir.ciocoiu@gmail.com>
2020-06-25 12:47:47 +0200

    order route modified with more queries to db

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,51 +1,118 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Order = require('../models/order');
+const Order = require("../models/order");
+const Product = require("../models/product");
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then((docs) => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map((doc) => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/orders." + doc._id,
+            },
+          };
+        }),
+      });
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
-router.post('/', (req, res, next) => {
-    const order = new Order({
+router.post("/", (req, res, next) => {
+  // Check if we have a product for a given id
+  Product.findById(req.body.productId)
+    .then((prod) => {
+      // check against null
+      if (!prod) {
+        return res.status(404).json({
+          message: "Product not found",
+        });
+      }
+      const order = new Order({
         _id: mongoose.Types.ObjectId(),
         quantity: req.body.quantity,
-        product: req.body.productId
-    });
-    order.save()
+        product: req.body.productId,
+      });
+      return order.save();
+    })
     // then and catch are provided as default values but they are not real promises that is why we use exec
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.status(202).json(result)
+    // not that here we do not use exec beacuse save gives back a real promise
+    .then((result) => {
+      res.status(201).json({
+        message: "Order stored",
+        createdOrder: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity,
+        },
+        request: {
+          type: "GET",
+          url: "https://localhost:3000/orders/" + result._id,
+        },
+      });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Product not found",
+        error: err,
+      });
+    });
 });
 
 // Individual orders
-router.get('/:orderId', (req, res, next) => {
-    const id = req.params.orderId;
-    res.status(200).json({
-        message: 'Order details',
-        id: id
+router.get("/:orderId", (req, res, next) => {
+  Order.findById(req.params.orderId)
+    .exec()
+    .then((order) => {
+      res.status(200).json({
+        order: order,
+        request: {
+          message: "get all orders",
+          type: "GET",
+          url: "http://localhost:3000/orders",
+        },
+      });
     })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
-router.delete('/:orderId', (req, res, next) => {
-    const id = req.params.orderId;
-    res.status(200).json({
-        message: 'Order deleted',
-        id: id 
+router.delete("/:orderId", (req, res, next) => {
+  Order.remove(req.params.orderId)
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'Order deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost/3000/orders',
+                body: {productId: 'ID', quantity: 'Number'}
+            }
+        })
     })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
