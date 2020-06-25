50922e2cc99264c927f4dd4a7b7db61ee4eecfa3
DAVIDCIOCOIU95 <david.samir.ciocoiu@gmail.com>
2020-06-25 11:17:50 +0200

    Added mongoose connection with appropriate reqs on the controls)

---
+++ api/models/product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Schema: layout, the design of the object , the model will be the constructor
+const productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    price: {type: Number, required: true}
+});
+
+module.exports = mongoose.model('Product', productSchema);
--- app.js
+++ app.js
@@ -1,49 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders')
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+const mongoose = require("mongoose");
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+mongoose.connect(process.env.MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Set cors headers (this is only for browsers as they are the only ones to inforce the cors mechanism, postman is still able to make reqs)
 app.use((req, res, next) => {
-    // Adjust the response (we are not sending it but setting headers)
-    res.header('Access-Control-Allow-Origin', '*');
-    // Define the accepted headers sent along with the req
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-Width, Content-Type, Accept, Authorization')
-    // Browser will always send an option request first before any other
-    // This allows the browser if it can make specific reqs
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    // If we are not returning as above we have to call next
-    next();
+  // Adjust the response (we are not sending it but setting headers)
+  res.header("Access-Control-Allow-Origin", "*");
+  // Define the accepted headers sent along with the req
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-Width, Content-Type, Accept, Authorization"
+  );
+  // Browser will always send an option request first before any other
+  // This allows the browser if it can make specific reqs
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  // If we are not returning as above we have to call next
+  next();
 });
 
 // Use routes
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes)
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 //Error handling middleware: create the error if none of the routes above match with the user url req
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    // Forward the error requests
-    next(error);
+  const error = new Error("Not Found");
+  error.status = 404;
+  // Forward the error requests
+  next(error);
 });
 // Catch all kind of errors
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
